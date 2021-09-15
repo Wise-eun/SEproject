ca797abbf2739b2cbb76885d4c82faa84ecd0f02
Wise-eun <hee1871@naver.com>
2021-09-15 14:54:09 +0900

    new project

--- app/src/main/java/com/example/seproject/MainActivity.java
+++ app/src/main/java/com/example/seproject/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//dfdsf
+//dfdsfffgdfgdfg
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
