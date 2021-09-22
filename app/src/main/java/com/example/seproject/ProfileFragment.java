f50feec5b070ece7651f7469ea42639042fdfd16
dodms <dodms1006@naver.com>
2021-09-23 04:13:34 +0900

    make various screen

--- app/src/main/java/com/example/seproject/ProfileFragment.java
+++ app/src/main/java/com/example/seproject/ProfileFragment.java
@@ -1,8 +1,11 @@
 package com.example.seproject;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 import androidx.fragment.app.Fragment;
 
 public class ProfileFragment extends Fragment {
@@ -11,6 +14,8 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 }
